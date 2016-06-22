98bd981e1e3b5beb762efdf80bdb6487a03a840b
Oliver Ruehl <ruehl@serpens.uberspace.de>
2016-06-23 01:56:20 +0200

    Fixed pin sound

--- pin.gd
+++ pin.gd
@@ -5,14 +5,15 @@ extends RigidBody2D
 # var a=2
 # var b="textvar"
 
-func _ready():
-	set_process(true)
-	pass
-
 func _on_pin_body_enter( body ):
 	print("touched pin")
 	get_node("sfx").play("pinsound")
 	pass # replace with function body
+
+func _ready():
+	pass
+
+
 
 
 
