986db21dff00dd9a776ade8af56d6b72d027341c
Oliver Ruehl <ruehl@serpens.uberspace.de>
2016-06-23 02:24:00 +0200

    pin sounds

--- pin.gd
+++ pin.gd
@@ -5,8 +5,11 @@ extends RigidBody2D
 # var a=2
 # var b="textvar"
 
+
+
 func _on_pin_body_enter( body ):
 	print("touched pin")
+	#get_node("sfx").set_pitch_scale(2, 4)
 	get_node("sfx").play("pinsound")
 	pass # replace with function body
 
