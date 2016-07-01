6f0e98ad663152b558fd8b27b432bded55627d5e
Oliver Ruehl <ruehl@serpens.uberspace.de>
2016-07-01 13:58:18 +0200

    Ball

--- box.gd
+++ box.gd
@@ -9,9 +9,9 @@ func _on_box_body_enter( body ):
 	print("touched wood")
 	#get_node("sfx").set_pitch_scale(2, 4)
 	get_node("sfx").play("thunk")
-	get_node("/root/game/ball_wrapper/ball").set_mass(300)
-	get_node("/root/game/ball_wrapper/ball").set_friction(0)
-	get_node("/root/game/ball_wrapper/ball").set_bounce(0)
+	get_node("/root/game/ball").set_mass(300)
+	get_node("/root/game/ball").set_friction(0)
+	get_node("/root/game/ball").set_bounce(0)
 	#get_node("/root/game/ball").set_sleeping(true)
 	
 func _ready():
