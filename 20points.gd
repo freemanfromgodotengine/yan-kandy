3ffeb874b7240749dadbc3295a8cc02fd19215e0
Oliver Ruehl <ruehl@serpens.uberspace.de>
2016-07-03 21:20:07 +0200

    added HUD

--- 20points.gd
+++ 20points.gd
@@ -8,10 +8,12 @@ extends Area2D
 var score = 0
 
 func _ready():
-	
+	get_node("score_text").set_text(str(score))
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+
 
 
 
@@ -20,6 +22,6 @@ func _on_20_points_body_enter( body ):
 	if score == 0:
 		print("20 points")
 		score = 20
-		get_node("Label").set_text(str(score))
+		get_node("score_text").set_text(str(score))
 	print("sorry")
 	pass # replace with function body
