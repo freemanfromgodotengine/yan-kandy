8681813e9d21e18a763115e63c1a9d47093f6174
Freeman <emailforthework@gmail.com>
2016-07-04 13:12:53 +0200

    Adding a reverb to the pin_sounds
    Hushing the previous playing sample line of code and adding reverb to the sounds in the new line, but the SamplePlayer2D node in instanced pins needs to be changed into SamplePlayer node.

--- pin.gd
+++ pin.gd
@@ -13,13 +13,16 @@ func _on_pin_body_enter( body ):
 	print("touched pin")
 	
 	if is_in_group("box_area1"):
-		get_node("sfx").play("pin_sound_1")
+		#get_node("sfx").play("pin_sound_1")
+		get_node("sfx").set_reverb(get_node("sfx").play("pin_sound_1"), 2, 0.05) # this adds a reverb fx to the sound
 	
 	if is_in_group("box_area2"):
-		get_node("sfx").play("pin_sound_2")
-		
+		#get_node("sfx").play("pin_sound_2")
+		get_node("sfx").set_reverb(get_node("sfx").play("pin_sound_2"), 2, 0.05) # this adds a reverb fx to the sound
+	
 	if is_in_group("box_area3"):
-		get_node("sfx").play("pin_sound_3")
+		#get_node("sfx").play("pin_sound_3")
+		get_node("sfx").set_reverb(get_node("sfx").play("pin_sound_3"), 2, 0.05) # this adds a reverb fx to the sound
 	
 	else:
 		#randomize()
